0e6760f50a1193ade4bbf1041edb99710bd7b29c
Daniel CH Tan <dtch1997@users.noreply.github.com>
2024-01-20 17:52:14 +0000

    Restore original URL

--- src/jam/model_zoo.py
+++ src/jam/model_zoo.py
@@ -39,9 +39,10 @@ MODEL_URLS = {
         # "config_md5": "f1929b19202d85c0829e41f302007bcc",
     },
     "r3m/r3m-18": {
-        # Original URL from R3M
-        # "ckpt_url": "https://drive.google.com/uc?id=1A1ic-p4KtYlKXdXHcV2QV0cUzI4kn0u-",
-        "ckpt_url": "https://drive.google.com/uc?export=download&id=1FhMe4QOzLs0OVRww9mNBK54T6R6xiALt",
+        # URL from original repo
+        "ckpt_url": "https://drive.google.com/uc?id=1A1ic-p4KtYlKXdXHcV2QV0cUzI4kn0u-",
+        # self-hosted URL
+        # "ckpt_url": "https://drive.google.com/uc?export=download&id=1FhMe4QOzLs0OVRww9mNBK54T6R6xiALt",
         "ckpt_md5": "777854e2548f91480b0bd538a06ba017",
         # "config_url": "https://drive.google.com/uc?id=1nitbHQ-GRorxc7vMUiEHjHWP5N11Jvc6",
         # "config_md5": "e226d5115543dee07fd51a17c7866bc9",
